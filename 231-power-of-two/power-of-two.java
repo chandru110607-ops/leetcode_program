8c9ef4e80cb1294d959db92cc72807cc1d6ca46c
CHANDRU T <chandru110607@gmail.com>
2026-09-15 18:29:56 +0530

    Time: 1 ms (96.66%) | Memory: 42.5 MB (55.36%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+       for(int i=0;i<=31;i++){
+        long ans=(long)Math.pow(2,i);
+        if(ans==n){
+            return true;
+        }
+       }
+       return false;
+    }
+}
