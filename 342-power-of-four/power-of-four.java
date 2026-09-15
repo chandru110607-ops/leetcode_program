d5c7406c8c84f4fff298a8eb36cbebcc20e30ee6
CHANDRU T <chandru110607@gmail.com>
2026-09-15 18:31:57 +0530

    Time: 1 ms (90.10%) | Memory: 42.5 MB (51.94%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        for(int i=0;i<=31;i++){
+            long ans=(long)Math.pow(4,i);
+            if(ans==n){
+                return true;
+            }
+        }
+        return false;
+    }
+}
