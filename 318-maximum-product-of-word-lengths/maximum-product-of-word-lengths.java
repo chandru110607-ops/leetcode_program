a4ccab4346b98f492b896f2da09e3f943f40feb2
CHANDRU T <chandru110607@gmail.com>
2026-09-17 18:33:20 +0530

    Time: 1430 ms (9.32%) | Memory: 49.2 MB (75.63%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int count=0;
+        for(int i=0;i<words.length;i++){
+            for(int j=i+1;j<words.length;j++)
+            {
+               if(!hasCommonLetters(words[i],words[j])){
+                int pro=words[i].length()*words[j].length();
+                count=Math.max(count,pro);
+               }
+            }
+        }
+        return count;
+    }
+    private boolean hasCommonLetters(String w1, String w2) {
+        boolean[] letters = new boolean[26];
+        for (int i = 0; i < w1.length(); i++) {
+            letters[w1.charAt(i) - 'a'] = true;
+        }
+        for (int i = 0; i < w2.length(); i++) {
+            if (letters[w2.charAt(i) - 'a']) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
