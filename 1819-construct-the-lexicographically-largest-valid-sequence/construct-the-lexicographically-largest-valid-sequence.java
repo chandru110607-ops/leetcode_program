22c5c08acf43b90138b8c56532c6a7171477851d
CHANDRU T <chandru110607@gmail.com>
2026-09-24 08:11:10 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (83.89%) - LeetSync

---
+++ 1819-construct-the-lexicographically-largest-valid-sequence/construct-the-lexicographically-largest-valid-sequence.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int[] constructDistancedSequence(int n) {
+        int[] result = new int[2 * n - 1];
+        boolean[] used = new boolean[n + 1];
+        backtrack(result, used, n, 0);
+        return result;
+    }
+
+    private boolean backtrack(int[] result, boolean[] used, int n, int index) {
+        if (index == result.length) {
+            return true;
+        }
+
+        if (result[index] != 0) {
+            return backtrack(result, used, n, index + 1);
+        }
+
+        for (int i = n; i >= 1; i--) {
+            if (used[i]) {
+                continue;
+            }
+
+            if (i == 1) {
+                result[index] = 1;
+                used[1] = true;
+                if (backtrack(result, used, n, index + 1)) {
+                    return true;
+                }
+                result[index] = 0;
+                used[1] = false;
+            } else {
+                if (index + i < result.length && result[index + i] == 0) {
+                    result[index] = i;
+                    result[index + i] = i;
+                    used[i] = true;
+                    
+                    if (backtrack(result, used, n, index + 1)) {
+                        return true;
+                    }
+                    
+                    result[index] = 0;
+                    result[index + i] = 0;
+                    used[i] = false;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
